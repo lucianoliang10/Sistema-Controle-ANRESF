cd3a0a8f6eeb0a40d0d5e3aaee4b2cc237503a9b
lucianoliang10 <lucianoguan@hotmail.com>
2026-07-22 16:49:57 -0300

    Restaura script existente de responsável

--- supabase/atualizar-objeto-parecer-tecnico-conclusivo.sql
+++
@@ -1,4 +0,0 @@
--- Atualiza todas as etapas chamadas "Parecer Técnico Conclusivo" para o objeto padronizado.
-update public.etapas
-set objeto = 'Elaboração de Parecer Técnico Conclusivo'
-where lower(trim(nome_etapa)) = lower('Parecer Técnico Conclusivo');
